09eed04025e6075ebe72b4a26a1e816d4cdae4c2
Zachary Bush <zach@zmbush.com>
2013-05-06 04:16:24 -0700

    Add some test

--- spec/coffeelint_spec.rb
+++ spec/coffeelint_spec.rb
@@ -1,4 +1,10 @@
 require 'spec_helper'
 
 describe Coffeelint do
+  it 'should error with semicolon' do
+    results = Coffeelint.lint('apple;')
+    results.length.should == 1
+    result = results[0]
+    result['message'].should include 'trailing semicolon'
+  end
 end
