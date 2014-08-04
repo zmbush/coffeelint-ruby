f9d204f77fd1203b062b57de3f6820459aca0460
masarakki <masaki@hisme.net>
2014-08-04 16:12:32 +0900

    update for rspec-3.x

--- spec/coffeelint_spec.rb
+++ spec/coffeelint_spec.rb
@@ -3,30 +3,30 @@ require 'spec_helper'
 describe Coffeelint do
   it 'should error with semicolon' do
     results = Coffeelint.lint('apple;')
-    results.length.should == 1
+    expect(results.length).to eq 1
     result = results[0]
-    result['message'].should include 'trailing semicolon'
+    expect(result['message']).to include 'trailing semicolon'
   end
 
   it 'should be able to disable a linter' do
     results = Coffeelint.lint('apple;', :no_trailing_semicolons =>  { :level => "ignore" } )
-    results.length.should == 0
+    expect(results.length).to eq 0
   end
 
   it 'should be able to take a config file in the parameters' do
     File.open('/tmp/coffeelint.json', 'w') {|f| f.write(JSON.dump({:no_trailing_semicolons => { :level => "ignore" }})) }
     results = Coffeelint.lint('apple;', :config_file => "/tmp/coffeelint.json")
-    results.length.should == 0
+    expect(results.length).to eq 0
   end
 
   it 'should report missing fat arrow' do
     results = Coffeelint.lint "hey: ->\n  @bort()\n", :missing_fat_arrows => { :level => "error" }
-    results.length.should == 1
+    expect(results.length).to eq 1
   end
 
   it 'should report unnecessary fat arrow' do
     results = Coffeelint.lint "hey: =>\n  bort()\n", :no_unnecessary_fat_arrows => { :level => "error" }
-    results.length.should == 1
+    expect(results.length).to eq 1
   end
 
   it 'should report cyclomatic complexity' do
@@ -37,7 +37,7 @@ describe Coffeelint do
         7 and 8 and 9 and
         10 and 11
     EOF
-    results.length.should == 1
-    results[0]['name'].should == 'cyclomatic_complexity'
+    expect(results.length).to eq 1
+    expect(results[0]['name']).to eq 'cyclomatic_complexity'
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,4 @@ require 'rspec'
 require 'coffeelint'
 
 RSpec.configure do |config|
-  config.color_enabled = true
-  config.formatter = 'documentation'
 end
